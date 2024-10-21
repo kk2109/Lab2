bb75c44759c3058962436e1588ab64caf9f27d76
Sihui <sihuic.23@ichat.sp.edu.sg>
2024-10-21 15:33:36 +0800

    define the main entry point

--- lab2.py
+++ lab2.py
@@ -1,1 +1,7 @@
-print("ET0735 (devOps for AIoT) - lab 2 - Introduction to python")
+def main():
+    print("ET0735 (devOps for AIoT) - lab 2 - Introduction to python")
+    display_main_menu()
+    num_list=get_user_input()
+
+    if __name__ == "__main__":
+        main()
